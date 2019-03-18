ef57badeea42e57478e82e82fb15065bb27770bb
Tim Visher <tim@stitchdata.com>
2019-03-18 19:48:16 +0000

    Spike: Actually connect to the local db

--- spikes/connection-test/connection-test.py
+++ spikes/connection-test/connection-test.py
@@ -61,3 +61,41 @@
 #         Developer Edition (64-bit) on Linux (Ubuntu 16.04.4 LTS)                                                                                                            
 
 # (1 rows affected)
+
+import pyodbc
+server = 'localhost'
+database = 'SampleDB'
+username = 'sa'
+password = 'password123!'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+print ('Inserting a new row into table')
+#Insert Query
+tsql = "INSERT INTO Employees (Name, Location) VALUES (?,?);"
+with cursor.execute(tsql,'Jake','United States'):
+    print ('Successfuly Inserted!')
+
+
+#Update Query
+print ('Updating Location for Nikita')
+tsql = "UPDATE Employees SET Location = ? WHERE Name = ?"
+with cursor.execute(tsql,'Sweden','Nikita'):
+    print ('Successfuly Updated!')
+
+
+#Delete Query
+print ('Deleting user Jared')
+tsql = "DELETE FROM Employees WHERE Name = ?"
+with cursor.execute(tsql,'Jared'):
+    print ('Successfuly Deleted!')
+
+
+#Select Query
+print ('Reading data from table')
+tsql = "SELECT Name, Location FROM Employees;"
+with cursor.execute(tsql):
+    row = cursor.fetchone()
+    while row:
+        print (str(row[0]) + " " + str(row[1]))
+        row = cursor.fetchone()
