d75c41fea2b2b81ab7845b6a3778708e8fa9cdf8
Dan Mosora <dan@stitchdata.com>
2019-05-08 22:08:16 +0000

    Pass-through discovery mode

--- src/tap_mssql/core.clj
+++ src/tap_mssql/core.clj
@@ -2,12 +2,21 @@
   (:require [clojure.tools.logging :as logger]
             [clojure.tools.nrepl.server :as nrepl-server]
             [clojure.tools.cli :as cli]
+            [clojure.java.io :as io]
+            [cheshire.core :as json]
             )
   (:gen-class))
 
 
 (def cli-options
-  [[nil "--repl" "REPL Mode"]
+  [["-d" "--discover" "Discovery Mode"]
+   [nil "--repl" "REPL Mode"]
+   [nil "--config CONFIG" "Config File"
+    :parse-fn #(json/parse-stream (io/reader %) true)]
+   [nil "--catalog CATALOG" "Singer Catalog File"
+    :parse-fn #(json/parse-stream (io/reader %) true)]
+   [nil "--state STATE" "Singer State File"
+    :parse-fn #(json/parse-stream (io/reader %) true)]
    ["-h" "--help"]])
 
 (defn nrepl-handler []
@@ -25,6 +34,13 @@
   (nrepl-server/start-server :bind "0.0.0.0"
                              :handler (nrepl-handler)))
 
+(defn do-discovery []
+  (log-infof "Starting discovery mode"))
+
+(defn do-sync [config catalog state]
+  (log-infof "Starting sync mode")
+  (throw (ex-info "Sync mode not yet implemented." {})))
+
 (defn -main [& args]
   (try
     (let [opts (cli/parse-opts args cli-options)
@@ -33,13 +49,23 @@
                             (.getLocalSocketAddress (:server-socket the-nrepl-server))))
       (spit ".nrepl-port" (:port the-nrepl-server))
 
+      (cond
+        discover
+        (do-discovery)
+
+        catalog
+        (do-sync config catalog state)
+
+        :else
+        ;; FIXME: (show-help)?
+        nil)
 
       (if repl
         (.start (Thread. #((loop []
-                       (Thread/sleep 1000)
-                       (recur)))))
+                             (Thread/sleep 1000)
+                             (recur)))))
         (nrepl-server/stop-server the-nrepl-server)))
-   (catch Exception ex
+    (catch Exception ex
       (dorun (map #(logger/fatal %)
                   (clojure.string/split (str ex) #"\n")))
       (throw ex))))
