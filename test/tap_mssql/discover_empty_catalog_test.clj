29c059d51a34d4fad9df58718de9f6ba0abd55e1
Tim Visher <tim@stitchdata.com>
2019-05-15 16:55:07 +0000

    Use with-out-and-err-to-dev-null from test

--- test/tap_mssql/discover_empty_catalog_test.clj
+++ test/tap_mssql/discover_empty_catalog_test.clj
@@ -1,5 +1,6 @@
 (ns tap-mssql.discover-empty-catalog-test
   (:require [clojure.test :refer [is deftest]]
+            [clojure.java.io :as io]
             [tap-mssql.core :refer :all]))
 
 (def test-db-config
@@ -7,5 +8,15 @@
    :user (System/getenv "STITCH_TAP_MSSQL_TEST_DB_USER")
    :password (System/getenv "STITCH_TAP_MSSQL_TEST_DB_PASSWORD")})
 
+(defmacro with-out-and-err-to-dev-null
+  [& body]
+  `(let [null-out# (io/writer
+                    (proxy [java.io.OutputStream] []
+                      (write [& args#])))]
+     (binding [*err* null-out#
+               *out* null-out#]
+       ~@body)))
+
 (deftest ^:integration verify-mssql-version
-  (is (do-discovery test-db-config)))
+  (is (with-out-and-err-to-dev-null
+        (do-discovery test-db-config))))
