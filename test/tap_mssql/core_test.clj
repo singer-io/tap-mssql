2f104a9757d7b5a3d61d9249f8edb0e1b8f5aaad
Tim Visher <tim@stitchdata.com>
2019-05-17 18:12:25 +0000

    Discover int, bigint, smallint, and tinyint datatypes

--- test/tap_mssql/core_test.clj
+++ test/tap_mssql/core_test.clj
@@ -5,30 +5,54 @@
 (deftest add-int-column-to-catalog
   (is (= {:streams
           {"theologians"
-           {:stream "theologians"
+           {:stream        "theologians"
             :tap-stream-id "theologians"
-            :table-name "theologians"
-            :schema {:type "object"
-                     :properties {"name" {:type "integer"}
-                                  "year_of_death" {:type "integer"}}}
-            :metadata {}}
+            :table-name    "theologians"
+            :schema        {:type       "object"
+                            :properties {"name"          {:type    "integer"
+                                                          :minimum -2147483648
+                                                          :maximum 2147483647}
+                                         "year_of_death" {:type    "integer"
+                                                          :minimum -2147483648
+                                                          :maximum 2147483647}}}
+            :metadata      {}}
            "revivalists"
-           {:stream "revivalists"
+           {:stream        "revivalists"
             :tap-stream-id "revivalists"
-            :table-name "revivalists"
-            :schema {:type "object"
-                     :properties {"name" {:type "integer"}
-                                  "year_of_death" {:type "integer"}}}
-            :metadata {}}}}
-         (reduce add-column empty-catalog [{:table_name "theologians"
-                                            :table_cat "test"
-                                            :column_name "name"}
-                                           {:table_name "theologians"
-                                            :table_cat "test"
-                                            :column_name "year_of_death"}
-                                           {:table_name "revivalists"
-                                            :table_cat "test"
-                                            :column_name "name"}
-                                           {:table_name "revivalists"
-                                            :table_cat "test"
-                                            :column_name "year_of_death"}]))))
+            :table-name    "revivalists"
+            :schema        {:type       "object"
+                            :properties {"name"          {:type    "integer"
+                                                          :minimum -2147483648
+                                                          :maximum 2147483647}
+                                         "year_of_death" {:type    "integer"
+                                                          :minimum -2147483648
+                                                          :maximum 2147483647}}}
+            :metadata      {}}}}
+         (reduce add-column empty-catalog [{:table_name  "theologians"
+                                            :table_cat   "test"
+                                            :column_name "name"
+                                            :type_name "int"}
+                                           {:table_name  "theologians"
+                                            :table_cat   "test"
+                                            :column_name "year_of_death"
+                                            :type_name "int"}
+                                           {:table_name  "revivalists"
+                                            :table_cat   "test"
+                                            :column_name "name"
+                                            :type_name "int"}
+                                           {:table_name  "revivalists"
+                                            :table_cat   "test"
+                                            :column_name "year_of_death"
+                                            :type_name "int"}]))))
+
+(comment
+  ;; Run all loaded tests
+  (do
+    (require '[clojure.string :as string])
+    (apply clojure.test/run-tests (->> (all-ns)
+                                       (map ns-name)
+                                       (filter #(string/starts-with? % "tap-mssql."))
+                                       (filter #(string/ends-with? % "-test")))))
+
+  (clojure.test/run-tests *ns*)
+  )
