975998a5dacd3e43d820c9b69d7c426d2bd4aa7e
Dan Mosora <dan@stitchdata.com>
2019-06-05 22:30:34 +0000

    WIP test matrix pattern

--- test/tap_mssql/discover_empty_catalog_test.clj
+++ test/tap_mssql/discover_empty_catalog_test.clj
@@ -6,7 +6,10 @@
             [clojure.string :as string]
             [tap-mssql.core :refer :all]
             [tap-mssql.test-utils :refer [with-out-and-err-to-dev-null
-                                          test-db-config]]))
+                                          test-db-config
+                                          test-db-configs
+                                          def-matrix-tests
+                                          *test-db-config*]]))
 
 (defn get-destroy-database-command
   [database]
@@ -31,9 +34,17 @@
     (create-test-db)
     (f)))
 
+;; TODO: These functions should use the config from the binding somehow
+;;       Otherwise, the database won't be set up for the tests
 (use-fixtures :each test-db-fixture)
 
-(deftest ^:integration verify-mssql-version
+;; TODO: For some reason these are failing. 2017 seems to be using the 2014 definition? (5:45ish)
+;; -- Follow-up: This seems to be working now, inexplicably. (6:24, no changes, just redef'd)
+(def-matrix-tests verify-mssql-version test-db-configs
+  (is (nil? (do-discovery *test-db-config*))
+      "Discovery ran succesfully and did not throw an exception"))
+
+#_(deftest ^:integration verify-mssql-version
   (is (nil?
        (do-discovery test-db-config))
       "Discovery ran succesfully and did not throw an exception"))
@@ -41,3 +52,9 @@
 (deftest ^:integration verify-empty-catalog
   (is (= empty-catalog (discover-catalog test-db-config))
       "Databases without any tables (like empty_database) do not show up in the catalog"))
+
+(comment
+  (map (comp (partial ns-unmap *ns*) #(.sym %)) (filter (comp :test meta) (vals (ns-publics *ns*))))
+
+  (ns-unmap *ns* (.sym #'tap-mssql.discover-empty-catalog-test/*test-db-config*))
+  )
