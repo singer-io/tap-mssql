6d4af2fd494699b005c8b4d87006d3528145db89
Dan Mosora <dan@stitchdata.com>
2019-06-07 19:52:43 +0000

    Saving progress on test matrix to be fleshed out

--- test/tap_mssql/discover_empty_catalog_test.clj
+++ test/tap_mssql/discover_empty_catalog_test.clj
@@ -9,6 +9,7 @@
                                           test-db-config
                                           test-db-configs
                                           def-matrix-tests
+                                          with-matrix-assertions
                                           *test-db-config*]]))
 
 (defn get-destroy-database-command
@@ -17,7 +18,8 @@
 
 (defn maybe-destroy-test-db
   []
-  (let [destroy-database-commands (->> (get-databases test-db-config)
+  (let [test-db-config (or *test-db-config* test-db-config)
+        destroy-database-commands (->> (get-databases test-db-config)
                                        (filter non-system-database?)
                                        (map get-destroy-database-command))]
     (let [db-spec (config->conn-map test-db-config)]
@@ -25,7 +27,8 @@
 
 (defn create-test-db
   []
-  (let [db-spec (config->conn-map test-db-config)]
+  (let [test-db-config *test-db-config*
+        db-spec (config->conn-map test-db-config)]
     (jdbc/db-do-commands db-spec ["CREATE DATABASE empty_database"])))
 
 (defn test-db-fixture [f]
@@ -34,27 +37,26 @@
     (create-test-db)
     (f)))
 
-;; TODO: These functions should use the config from the binding somehow
-;;       Otherwise, the database won't be set up for the tests
-(use-fixtures :each test-db-fixture)
-
-;; TODO: For some reason these are failing. 2017 seems to be using the 2014 definition? (5:45ish)
-;; -- Follow-up: This seems to be working now, inexplicably. (6:24, no changes, just redef'd)
-(def-matrix-tests verify-mssql-version test-db-configs
+#_(def-matrix-tests verify-mssql-version test-db-configs test-db-fixture
   (is (nil? (do-discovery *test-db-config*))
       "Discovery ran succesfully and did not throw an exception"))
 
-#_(deftest ^:integration verify-mssql-version
-  (is (nil?
-       (do-discovery test-db-config))
-      "Discovery ran succesfully and did not throw an exception"))
+(deftest verify-mssql-version
+  (with-matrix-assertions test-db-configs test-db-fixture
+    (is (nil? (do-discovery *test-db-config*))
+        "Discovery ran succesfully and did not throw an exception")))
 
 (deftest ^:integration verify-empty-catalog
-  (is (= empty-catalog (discover-catalog test-db-config))
-      "Databases without any tables (like empty_database) do not show up in the catalog"))
+  (with-matrix-assertions test-db-configs test-db-fixture
+    (is (= empty-catalog (discover-catalog *test-db-config*))
+       "Databases without any tables (like empty_database) do not show up in the catalog")))
 
 (comment
+  ;; TODO helper functions?
+  ;; Clear all tests from namespace
   (map (comp (partial ns-unmap *ns*) #(.sym %)) (filter (comp :test meta) (vals (ns-publics *ns*))))
+  ;; Clear entire namespace
+  (map (comp (partial ns-unmap *ns*) #(.sym %)) (vals (ns-publics *ns*)))
 
   (ns-unmap *ns* (.sym #'tap-mssql.discover-empty-catalog-test/*test-db-config*))
   )
