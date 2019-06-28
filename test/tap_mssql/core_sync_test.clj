a25d57a3335d203b1a292e51a3ee41f55fc44771
Nick McCoy <33731945+nick-mccoy@users.noreply.github.com>
2019-06-28 09:33:10 -0400

    Empty schema for unsupported columns (#15)
    * empty schemas for unsupported props
    
    * dont add unsupported key props to table-key-properties metadata
    
    * Use schema in query (#16)
    
    * use schema in sql querys
    
    * update tests to use schema in sql querys
    
    * join lines

--- test/tap_mssql/core_sync_test.clj
+++ test/tap_mssql/core_sync_test.clj
@@ -32,7 +32,7 @@
                          [(jdbc/create-table-ddl
                            "composite_key_table"
                            [[:id "uniqueidentifier NOT NULL DEFAULT NEWID()"]
-                            [:second_id "int NOT NULL IDENTITY"]
+                            [:second_id "int NOT NULL"]
                             [:value "int"]
                             ["primary key (id, second_id)"]])])
     (jdbc/db-do-commands (assoc db-spec :dbname "full_table_sync_test")
@@ -48,27 +48,27 @@
 
 (deftest build-sync-query-test
   (is (thrown? AssertionError
-               (build-sync-query "craftsmanship-dbo-mahogany" "mahogany" [] {})))
+               (build-sync-query "craftsmanship-dbo-mahogany" "dbo" "mahogany" [] {})))
   ;; No bookmark, no pk = Full Table sync query
-  (is (= ["SELECT legs, tabletop, leaf FROM mahogany"]
-         (build-sync-query "craftsmanship-dbo-mahogany" "mahogany" ["legs", "tabletop", "leaf"] {})))
+  (is (= ["SELECT legs, tabletop, leaf FROM dbo.mahogany"]
+         (build-sync-query "craftsmanship-dbo-mahogany" "dbo" "mahogany" ["legs", "tabletop", "leaf"] {})))
   ;; No bookmark, yes pk = First FT Interruptible query
-  (is (= '("SELECT legs, tabletop, leaf FROM mahogany WHERE legs <= ? AND leaf <= ? ORDER BY legs, leaf"
+  (is (= '("SELECT legs, tabletop, leaf FROM dbo.mahogany WHERE legs <= ? AND leaf <= ? ORDER BY legs, leaf"
           4
           "birch")
-         (build-sync-query "craftsmanship-dbo-mahogany" "mahogany" ["legs", "tabletop", "leaf"]
+         (build-sync-query "craftsmanship-dbo-mahogany" "dbo" "mahogany" ["legs", "tabletop", "leaf"]
                            {"bookmarks" {"craftsmanship-dbo-mahogany" {"max_pk_values" {"legs" 4 "leaf" "birch"}}}})))
   ;; Bookmark, no pk = ??? Invalid state
   (is (thrown? AssertionError
-               (build-sync-query "craftsmanship-dbo-mahogany" "mahogany" ["legs", "tabletop", "leaf"]
+               (build-sync-query "craftsmanship-dbo-mahogany" "dbo" "mahogany" ["legs", "tabletop", "leaf"]
                                  {"bookmarks" {"craftsmanship-dbo-mahogany" {"last_pk_fetched" {"legs" 2 "leaf" "balsa"}}}})))
   ;; Bookmark and PK = Resuming Full Table Sync
-  (is (= '("SELECT legs, tabletop, leaf FROM mahogany WHERE legs >= ? AND leaf >= ? AND legs <= ? AND leaf <= ? ORDER BY legs, leaf"
+  (is (= '("SELECT legs, tabletop, leaf FROM dbo.mahogany WHERE legs >= ? AND leaf >= ? AND legs <= ? AND leaf <= ? ORDER BY legs, leaf"
           2
           "balsa"
           4
           "birch")
-         (build-sync-query "craftsmanship-dbo-mahogany" "mahogany" ["legs", "tabletop", "leaf"]
+         (build-sync-query "craftsmanship-dbo-mahogany" "dbo" "mahogany" ["legs", "tabletop", "leaf"]
                            {"bookmarks"
                             {"craftsmanship-dbo-mahogany"
                              {"last_pk_fetched" {"legs" 2 "leaf" "balsa"}
@@ -89,7 +89,7 @@
                                  {} )))
     ;; Has primary key, no record Keys, no primary key bookmark
     (is (=
-         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id FROM CHANGETABLE (CHANGES basic_table, 0) as c LEFT JOIN basic_table ON c.id=basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts ORDER BY c.SYS_CHANGE_VERSION, c.id"]
+         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id FROM CHANGETABLE (CHANGES dbo.basic_table, 0) as c LEFT JOIN dbo.basic_table ON c.id=dbo.basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts ORDER BY c.SYS_CHANGE_VERSION, c.id"]
          (build-log-based-sql-query
           (update-in (discover-catalog test-db-config)
                      ["streams" "full_table_sync_test-dbo-basic_table" "metadata" "properties" "value"]
@@ -99,7 +99,7 @@
           {"bookmarks" {"full_table_sync_test-dbo-basic_table" {"current_log_version" 0}}})))
     ;; Has PK, No Selected Fields, Has Bookmark
     (is (=
-         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id FROM CHANGETABLE (CHANGES basic_table, 0) as c LEFT JOIN basic_table ON c.id=basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts WHERE c.SYS_CHANGE_VERSION = 0 AND c.id >= ? ORDER BY c.SYS_CHANGE_VERSION, c.id" "foo"]
+         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id FROM CHANGETABLE (CHANGES dbo.basic_table, 0) as c LEFT JOIN dbo.basic_table ON c.id=dbo.basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts WHERE c.SYS_CHANGE_VERSION = 0 AND c.id >= ? ORDER BY c.SYS_CHANGE_VERSION, c.id" "foo"]
          (build-log-based-sql-query
           (update-in (discover-catalog test-db-config)
                      ["streams" "full_table_sync_test-dbo-basic_table" "metadata" "properties" "value"]
@@ -112,7 +112,7 @@
              "last_pk_fetched"     {"id" "foo"}}}})))
     ;; Has primary key, selected fields, no primary key bookmark
     (is (=
-         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id, basic_table.value FROM CHANGETABLE (CHANGES basic_table, 0) as c LEFT JOIN basic_table ON c.id=basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts ORDER BY c.SYS_CHANGE_VERSION, c.id"]
+         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id, dbo.basic_table.value FROM CHANGETABLE (CHANGES dbo.basic_table, 0) as c LEFT JOIN dbo.basic_table ON c.id=dbo.basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts ORDER BY c.SYS_CHANGE_VERSION, c.id"]
          (build-log-based-sql-query
           (discover-catalog test-db-config)
           "full_table_sync_test-dbo-basic_table"
@@ -122,7 +122,7 @@
 
     ;; Has primary key, selected fields, primary key bookmark
     (is (=
-         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id, basic_table.value FROM CHANGETABLE (CHANGES basic_table, 0) as c LEFT JOIN basic_table ON c.id=basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts WHERE c.SYS_CHANGE_VERSION = 0 AND c.id >= ? ORDER BY c.SYS_CHANGE_VERSION, c.id" "foo"]
+         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id, dbo.basic_table.value FROM CHANGETABLE (CHANGES dbo.basic_table, 0) as c LEFT JOIN dbo.basic_table ON c.id=dbo.basic_table.id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts WHERE c.SYS_CHANGE_VERSION = 0 AND c.id >= ? ORDER BY c.SYS_CHANGE_VERSION, c.id" "foo"]
          (build-log-based-sql-query
           (discover-catalog test-db-config)
           "full_table_sync_test-dbo-basic_table"
@@ -132,7 +132,7 @@
              "last_pk_fetched"     {"id" "foo"}}}})))
     ;; Has composite primary keys, selected fields, bookmarks for both pks
     (is (=
-         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id, c.second_id, composite_key_table.value FROM CHANGETABLE (CHANGES composite_key_table, 0) as c LEFT JOIN composite_key_table ON c.id=composite_key_table.id AND c.second_id=composite_key_table.second_id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts WHERE c.SYS_CHANGE_VERSION = 0 AND c.id >= ? AND c.second_id >= ? ORDER BY c.SYS_CHANGE_VERSION, c.id, c.second_id" "foo" "bar"]
+         ["SELECT c.SYS_CHANGE_VERSION, c.SYS_CHANGE_OPERATION, tc.commit_time, c.id, c.second_id, dbo.composite_key_table.value FROM CHANGETABLE (CHANGES dbo.composite_key_table, 0) as c LEFT JOIN dbo.composite_key_table ON c.id=dbo.composite_key_table.id AND c.second_id=dbo.composite_key_table.second_id LEFT JOIN sys.dm_tran_commit_table tc on c.sys_change_version = tc.commit_ts WHERE c.SYS_CHANGE_VERSION = 0 AND c.id >= ? AND c.second_id >= ? ORDER BY c.SYS_CHANGE_VERSION, c.id, c.second_id" "foo" "bar"]
          (build-log-based-sql-query
           (discover-catalog test-db-config)
           "full_table_sync_test-dbo-composite_key_table"
