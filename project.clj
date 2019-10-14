647ffb234c7f9c451b5d9c7856f02574d9cf6e11
Collin Simon <csimon@talend.com>
2019-10-14 14:50:37 +0000

    Bump to v1.3.0 and update changelog

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tap-mssql
-  "1.2.3"
+  "1.3.0"
   :description "Singer.io tap for extracting data from a Microsft SQL Server "
   :url "https://github.com/stitchdata/tap-mssql"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
