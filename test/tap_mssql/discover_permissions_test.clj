d764410c96bac4b14f8bbf707aad7fe2c73f7721
Dan Mosora <dan@stitchdata.com>
2020-05-15 19:31:17 +0000

    Try/catch for this specific error and fix test

--- test/tap_mssql/discover_permissions_test.clj
+++ test/tap_mssql/discover_permissions_test.clj
@@ -16,9 +16,9 @@
 
 (defn maybe-destroy-test-db
   []
-  (let [destroy-database-commands (->> [{:table_cat "empty_database"}
+  (let [destroy-database-commands (->> [{:table_cat "not_authorized_database"}
                                         {:table_cat "database_with_a_table"}
-                                        {:table_cat "database_with_table_valued_function"}]
+                                        {:table_cat "not_authorized_database_too"}]
                                        (filter catalog/non-system-database?)
                                        (map get-destroy-database-command))]
     (let [db-spec (config/->conn-map test-db-config)]
@@ -27,20 +27,17 @@
 (defn create-test-db
   []
   (let [db-spec (config/->conn-map test-db-config)]
-    (jdbc/db-do-commands db-spec ["CREATE DATABASE empty_database"
+    (jdbc/db-do-commands db-spec ["CREATE DATABASE not_authorized_database"
                                   "CREATE DATABASE database_with_a_table"
-                                  "CREATE DATABASE database_with_table_valued_function"])
+                                  "CREATE DATABASE not_authorized_database_too"])
+    ;; Create Tables
     (jdbc/db-do-commands (assoc db-spec :dbname "database_with_a_table")
                          [(jdbc/create-table-ddl :empty_table [[:id "int"]])])
-    (when (empty? (jdbc/query (assoc db-spec :dbname "database_with_a_table")
-                       "SELECT principal_id FROM sys.server_principals WHERE name = 'SingerTestUser'"))
-      (jdbc/db-do-commands (assoc db-spec :dbname "database_with_a_table")
-                           ["CREATE LOGIN SingerTestUser WITH PASSWORD = 'ABCD12345$%'"]))
-    (when (empty? (jdbc/query (assoc db-spec :dbname "database_with_a_table")
-                       "SELECT principal_id FROM sys.database_principals WHERE name = 'SingerTestUser'"))
-      (jdbc/db-do-commands (assoc db-spec :dbname "database_with_a_table")
-                           ["CREATE USER SingerTestUser FOR LOGIN SingerTestUser"
-                            "GRANT SELECT ON dbo.empty_table TO SingerTestUser"]))
+    (jdbc/db-do-commands (assoc db-spec :dbname "not_authorized_database")
+                         [(jdbc/create-table-ddl :empty_table [[:id "int"]])])
+    (jdbc/db-do-commands (assoc db-spec :dbname "not_authorized_database_too")
+                         [(jdbc/create-table-ddl :empty_table [[:id "int"]])])
+    ;; Create view/tvf for fun
     (jdbc/db-do-commands (assoc db-spec :dbname "database_with_a_table")
                          ["CREATE VIEW empty_table_ids
                            AS
@@ -52,7 +49,18 @@
                            BEGIN
                                INSERT INTO @result VALUES(@input_value + 1)
                                RETURN
-                           END"])))
+                           END"])
+    ;; Create User if not exists
+    (when (empty? (jdbc/query (assoc db-spec :dbname "database_with_a_table")
+                              "SELECT principal_id FROM sys.server_principals WHERE name = 'SingerTestUser'"))
+      (jdbc/db-do-commands (assoc db-spec :dbname "database_with_a_table")
+                           ["CREATE LOGIN SingerTestUser WITH PASSWORD = 'ABCD12345$%'"]))
+    (when (empty? (jdbc/query (assoc db-spec :dbname "database_with_a_table")
+                              "SELECT principal_id FROM sys.database_principals WHERE name = 'SingerTestUser'"))
+      (jdbc/db-do-commands (assoc db-spec :dbname "database_with_a_table")
+                           ["CREATE USER SingerTestUser FOR LOGIN SingerTestUser"
+                            "GRANT SELECT ON dbo.empty_table TO SingerTestUser"
+                            "GRANT SELECT ON dbo.empty_table_ids TO SingerTestUser"]))))
 
 (defn test-db-fixture [f]
   (with-out-and-err-to-dev-null
@@ -75,7 +83,17 @@
         (stream-names "empty_table_ids")))
   ;; Table-Valued functions should not be discovered
   (is (nil? (let [stream-names (set (map #(get % "stream") (vals ((catalog/discover (assoc test-db-config
-                                                                                     "user" "SingerTestUser"
-                                                                                     "password" "ABCD12345$%"))
+                                                                                           "user" "SingerTestUser"
+                                                                                           "password" "ABCD12345$%"))
                                                                   "streams"))))]
-              (stream-names "table_valued_test")))))
+              (stream-names "table_valued_test"))))
+
+  ;; Should not discover tables in non-permitted databases
+  (is (let [discovered-dbs (->> (get (catalog/discover (assoc test-db-config
+                                                              "user" "SingerTestUser"
+                                                              "password" "ABCD12345$%")) "streams")
+                                (map (fn [[_ schema]] (get-in schema ["metadata" "database-name"])))
+                                set)]
+        (empty? (clojure.set/intersection
+                 discovered-dbs
+                 #{"not_authorized_database" "not_authorized_database_too"})))))
