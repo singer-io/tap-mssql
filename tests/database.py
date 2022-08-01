d6c24ba820f2419b2c26984c3e4d615c9fbf1bf9
kspeer <kspeer@stitchdata.com>
2022-08-01 15:03:26 +0000

    remove unnecessary database utils changes

--- tests/database.py
+++ tests/database.py
@@ -122,7 +122,7 @@ def drop_database(db_name):
     return ["DROP DATABASE IF EXISTS {};".format(db_name)]
 
 
-def create_database(db_name, collation: str = None, user_permissions: bool = True):
+def create_database(db_name, collation: str = None):
     """
     CREATE DATABASE database_name
     [ CONTAINMENT = { NONE | PARTIAL } ]
@@ -145,7 +145,7 @@ def create_database(db_name, collation: str = None, user_permissions: bool = Tru
 
     # add user to databaseprint(query)
     return_value.extend(use_db(db_name))
-    if USERNAME.upper() != "SA" and user_permissions:
+    if USERNAME.upper() != "SA":
         return_value.extend(create_user(USERNAME))
     return return_value
 
