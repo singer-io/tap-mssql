5ab8192b2c91a48a84900a20a2d31b795782f3b9
prijendev <prijen.khokhani@qlik.com>
2026-07-29 09:37:46 +0530

    move import clause after require

--- test/tap_mssql/sync_full_table_test.clj
+++ test/tap_mssql/sync_full_table_test.clj
@@ -1,5 +1,4 @@
 (ns tap-mssql.sync-full-table-test
-  (:import  [microsoft.sql.DateTimeOffset])
   (:require [tap-mssql.catalog :as catalog]
             [tap-mssql.config :as config]
             [clojure.test :refer [is deftest]]
@@ -12,7 +11,8 @@
             [tap-mssql.test-utils :refer [with-out-and-err-to-dev-null
                                           test-db-config
                                           test-db-configs
-                                          with-matrix-assertions]]))
+                                          with-matrix-assertions]])
+  (:import [microsoft.sql.DateTimeOffset]))
 
 (defn get-destroy-database-command
   [database]
